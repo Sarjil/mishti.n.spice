c6526fb23d24db9c169dd3aae332a157cc5ac3d6
sarjilmiah <sarjilmiah@gmail.com>
2020-10-08 14:40:35 -0400

    created session actions

---
+++ frontend/util/user_api_util.js
@@ -0,0 +1,27 @@
+export const signup = (user) => {
+  return $.ajax({
+    method: "POST",
+    url: "/api/users",
+    data: { user },
+  });
+};
+
+export const fetchUser = (userId) =>
+  $.ajax({
+    method: "GET",
+    url: `/api/users/${userId}`,
+  });
+
+export const fetchUsers = () => {
+  return $.ajax({
+    method: "GET",
+    url: "/api/users",
+  });
+};
+
+export const updateUser = (user) =>
+  $.ajax({
+    method: "PATCH",
+    url: `/api/users/${user.id}`,
+    data: { user },
+  });
