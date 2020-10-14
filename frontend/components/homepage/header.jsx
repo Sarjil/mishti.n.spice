60168a401c4336d3ef8c2bb86c953f053755fc82
sarjilmiah <sarjilmiah@gmail.com>
2020-10-14 13:32:02 -0400

    continuing homepage

--- frontend/components/homepage/header.jsx
+++ frontend/components/homepage/header.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
-const Header = () =>(
-    <div className = "header">
-        <p className = "header-text"> Mishti N Spice</p>
-    </div>
-)
+class Header extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {navbar: "navbar-open"}
+        this.classChange = this.classChange.bind(this)
+    }
+
+    classChange(e){
+        e.preventDefault();
+        let nav = ((this.state.navbar )=== "navbar-open") ? "navbar-closed" : "navbar-open"
+        this.setState({navbar : nav})
+    };
+
+    render(){
+     return(
+        <div className = "header-component">
+            <div className = "header">
+                <div className = {this.state.navbar} onClick = {this.classChange}> = </div>
+                <div className = "header-text"> Mishti N Spice</div>
+                <div className = "placeholder">placeholder</div>
+            </div>
+            
+        </div>
+        )
+    }
+}
 
 export default Header; 
---
+++ frontend/components/homepage/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () =>(
+    <div className = "navbar-component">
+
+    </div>
+)
+
+export default Navbar; 
